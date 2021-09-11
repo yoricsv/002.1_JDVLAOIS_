532a3eaed8728b1e47d8090f8aa4618068e4b46b
Сергей <yoric@live.ru>
2021-09-11 13:43:00 +0300

    Add For-each loop

--- src/Loops.java
+++ src/Loops.java
@@ -2,6 +2,13 @@ public class Loops
 {
     public static void main(String... args)
     {
+        short[] daysOfMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+        // for-each
+        for(int i : daysOfMonth)
+        {
+            System.out.println(i);
+        }
 
     }
 }
