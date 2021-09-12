3093fd8249163e82a11078af6feedf6fc3c8a163
Сергей <yoric@live.ru>
2021-09-12 11:03:44 +0300

    Add type cast

--- src/Loops.java
+++ src/Loops.java
@@ -4,10 +4,35 @@ public class Loops
     {
         short[] daysOfMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
 
-        // for-each
+
+        System.out.print(
+                "These numbers were displayed by Loop For:\n"
+        );
+        String result = "";
+
+        // for loop
+        for(int i = 0; i < daysOfMonth.length; i++)
+        {
+            result += daysOfMonth[i];
+
+            if (i == daysOfMonth.length - 1)
+                result += ".";
+            else
+                result += ", ";
+        }
+
+        System.out.print(
+                result +
+                "\n\n" +
+                "These numbers were displayed by For-Each Loop:\n"
+        );
+
+
+        // for-each loop
         for(int i : daysOfMonth)
         {
-            System.out.println(i);
+            System.out.print(i);
+            System.out.print(" ");
         }
 
     }
--- src/TypesVariables.java
+++ src/TypesVariables.java
@@ -2,22 +2,27 @@ public class TypesVariables
 {
     public static void main(String... argc)
     {
-        // declaring character
-        boolean    flag = false;        // Range: "true" or "false" Size: 1 bit
+        // Logical types
+        boolean        flag = false;        // Range: "true" or "false" Size: 1 bit
 
-        byte    byteVar = 4;            // Range: "-128 ... 127" Size: 8 bit (1 Byte)
-        byte       bVar = 126;
+        // Number types
+        byte        byteVar = 4;            // Range: "-128 ... 127" Size: 8 bit (1 Byte)
+        byte           bVar = 126;
 
-        char    charVar = 'G';          // Range: "ASCII 0 ... 256" Size: 16 bit (2 Bytes)
-        char    endLine = '\n';         // Char type can contain of the escape consequences
+        short      shortVar = 56;           // Range: "-32768 ... 32767" Size: 16 bit (2 Bytes)
 
-        short  shortVar = 56;           // Range: "-32768 ... 32767" Size: 16 bit (2 Bytes)
+        int          intVar = 89;           // Range: "-32768 ... 32767"  Size: 32 bit (4 Bytes)
 
-        int      intVar = 89;           // Range: "-32768 ... 32767"  Size: 32 bit (4 Bytes)
+        long        longVar = 15424;        // Range: "-9223372036854775808 ... 9223372036854775807"
+        long      castToInt = 15424;        // Size: 64 bit (8 Bytes)
 
-        long    longVar = 15424;        // Range: "-9223372036854775808 ... 9223372036854775807" Size: 64 bit (8 Bytes)
+        // Symbolic types
+        char        charVar = 'G';          // Range: "ASCII 0 ... 256" Size: 16 bit (2 Bytes)
+        char        endLine = '\n';         // Char type can contain of the escape consequences
 
-        double  doubleV = 4.355453532;  // Range: "up to 16 decimal digits"  Size: 64 bit (8 Bytes)
+        // Real types
+        double      doubleV = 4.355453532;  // Range: "up to 16 decimal digits"  Size: 64 bit (8 Bytes)
+        double  castToFloat = 26.35;
 
     /*
      ************************************************
@@ -32,7 +37,17 @@ public class TypesVariables
         float  floatVar = 4.7333434f;
         float      summ = a + b;
 
+        // Manual type casting
+        float    radius = (float) castToFloat;
+        int        days =   (int) castToInt;
 
+        // Reverse type casting
+        double radiusInDouble = radius;                                 // if the type is less than needed,
+                                                                        // Java does an automatic casting
+
+        int       wholeRadius = (int) radiusInDouble;                   // get whole part
+        double  wipeWholePart = radiusInDouble - (double) wholeRadius;  // get fractional part
+        int  fractionalRadius = (int) (wipeWholePart * 100);            // TODO: May add the ranks check
 
         summ++;
 
@@ -47,30 +62,42 @@ public class TypesVariables
                     "\nThis flag is activated!"
             );
 
-        System.out.println("\n\t" + bVar  ); //Output: 126
-        System.out.println("\t"   + bVar++); //Output: 126 [increment after -> firstly print(bVar) then add(1)]
         System.out.println(
+                "\n\t"   + bVar                         +   //Output: 126
+                 // if increment after variable
+                 // -> make PRINT(bVar) then ADD(1)
+                "\n\t"   + bVar++                       +   //Output: 126
+
                 "\n\t\t>>> WARNING: OVERFLOW!!! <<<"    +
                 "\nIt overflows here because byte can " +
-                "hold values from -128 to 127"
+                "hold values from -128 to 127"          +
+
+                // if increment before variable
+                // -> make ADD(1) then PRINT(bVar)
+                "\n\t"   + ++bVar                       +   //Output: -128
+                "\nLooping back within the range"       +
+                "\n\t"   + ++bVar                           //Output: -127
         );
-        System.out.println("\t"   + ++bVar); //Output: -128 [increment before -> firstly add(1) then print(bVar)]
-        System.out.println(
-                "Looping back within the range"
-        );
-        System.out.println("\t"   + ++bVar); //Output: -127
 
         System.out.println(endLine  + "" + summ    );   //Output: 8.7
 
-        System.out.println("\n--------------------");
-        System.out.println("\tbool: "    + flag    );   //Output: false
-        System.out.println("\tbyte: "    + byteVar );   //Output: 4
-        System.out.println("\tchar: "    + charVar );   //Output: G
-        System.out.println("\tshort: "   + shortVar);   //Output: 56
-        System.out.println("\tinteger: " + intVar  );   //Output: 89
-        System.out.println("\tlong: "    + longVar );   //Output: 15424
-        System.out.println("\tdouble: "  + doubleV );   //Output: 4.355453532
-        System.out.println("\tfloat: "   + floatVar);   //Output: 4.7333436
+        System.out.println(
+                "\n--------------------" +
+                "\n\tbool: "    + flag     +   //Output: false
+                "\n\tbyte: "    + byteVar  +   //Output: 4
+                "\n\tchar: "    + charVar  +   //Output: G
+                "\n\tshort: "   + shortVar +   //Output: 56
+                "\n\tinteger: " + intVar   +   //Output: 89
+                "\n\tlong: "    + longVar  +   //Output: 15424
+                "\n\tdouble: "  + doubleV  +   //Output: 4.355453532
+                "\n\tfloat: "   + floatVar     //Output: 4.7333436
+        );
 
+        System.out.print(
+                "\nDISPLAY THE RADIUS, WHOLE AND FRACTIONAL PARTS:"  + "\n" +
+                "The radius is: "                 + radius           + "\n" +
+                "Whole part of the radius: "      + wholeRadius      + "\n" +
+                "Fractional part of the radius: " + fractionalRadius + "\n"
+        );
     }
 }
